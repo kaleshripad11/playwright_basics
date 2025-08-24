f87dffd83fded1f042f44b1dec36a656bdd07afa
ShripadKale <kaleshripad11@gmail.com>
2025-08-24 19:07:04 +0530

    Updated frame_handling code by adding code for frame object

--- day7_24_aug_2025/frame_handling.py
+++ day7_24_aug_2025/frame_handling.py
@@ -8,12 +8,15 @@ def frame_handling_demo():
 
         # Locate frame using page.frame_locator(locator)
         frame = page.frame_locator("#iframe-1")
+        frame_object = page.frame(name = "top-iframe")
+        print(f"This '{frame_object}' is a frame object")   # Output: This '<Frame name=top-iframe url='https://playwright.dev/'>' is a frame object
 
         # Access all chield elements of frame using frame variable
         frame.locator("a", has_text="Get Started").click()
 
         # Here used nth(index) function as there are more than one locators with expected text
         expect(frame.locator("span.breadcrumbs__link").nth(1)).to_have_text("Installation")
+
         browser.close()
         
 frame_handling_demo()
